dfc03548c681abe8d26d68cee89f0b2e60a61880
MSS <sarmadsaleem.333@gmail.com>
2024-10-14 15:25:28 +0500

    Fixed csv

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import warnings
 warnings.filterwarnings("ignore")
-
 import os
 import nltk
 nltk.download('stopwords')
 
-from pyresparser import ResumeParser
+from pyresparser import ResumeParser    
 
 # Parse the resume and extract data
-data = ResumeParser(os.path.join(os.getcwd(), 'MSS_FinalCV.pdf')).get_extracted_data()
+data = ResumeParser(os.path.join(os.getcwd(), 'Abubakar_Imran.pdf')).get_extracted_data()
 
 # Print the extracted data
 print(data)
